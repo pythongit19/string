22741acb18e240019c66d27ca0a21602d48761d3
shima98 <shima.azizimajd98@gmail.com>
2022-07-24 19:38:01 +0430

    add string

---
+++ string.py
@@ -0,0 +1,12 @@
+text = "we love python"
+cap = text.capitalize()
+print(cap)
+spl = text.split()
+print(spl)
+find = text.find('love')
+print(find)
+upper = text.upper()
+print(upper)
+lower = text.lower()
+print(lower)
+
